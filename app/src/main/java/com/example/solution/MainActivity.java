8e8979c26c84a784dd7b7fd208e0168e6f58099d
shazwendy <akothsharon19@gmail.com>
2019-05-07 10:09:06 +0300

    first commit

---
+++ app/src/main/java/com/example/solution/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.solution;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnFable1;
+    Button btnFable2;
+    Button btnFable3;
+    Button btnFable4;
+    Button btnFable5;
+    Button btnFable6;
+    Button btnFable7;
+    Button btnFable8;
+    Button btnFable9;
+    Button btnFable10;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnFable1 = findViewById(R.id.btnFable1);
+        btnFable2 = findViewById(R.id.btnFable2);
+        btnFable3 = findViewById(R.id.btnFable3);
+        btnFable4 = findViewById(R.id.btnFable4);
+        btnFable5 = findViewById(R.id.btnFable5);
+        btnFable6 = findViewById(R.id.btnFable6);
+        btnFable7 = findViewById(R.id.btnFable7);
+        btnFable8 = findViewById(R.id.btnFable8);
+        btnFable9 = findViewById(R.id.btnFable9);
+        btnFable10 = findViewById(R.id.btnFable10);
+
+        btnFable1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),Fable1_Activity.class));
+            }
+        });
+
+        btnFable2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),Fable2_Activity.class));
+            }
+        });
+        btnFable3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),Fable3_Activity.class));
+            }
+        });
+        btnFable4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),Fable4_Activity.class));
+            }
+        });
+        btnFable5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),Fable5_Activity.class));
+            }
+        });
+        btnFable6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),Fable6_Activity.class));
+            }
+        });
+        btnFable7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),Fable7_Activity.class));
+            }
+        });
+        btnFable8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),Fable8_Activity.class));
+            }
+        });
+        btnFable9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),Fable9_Activity.class));
+            }
+        });
+        btnFable10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),Fable10_Activity.class));
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
